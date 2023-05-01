897181279a6f707543451240806d84d2b9a936dd
UlyanaPyrohovska <ullasochka2015@gmail.com>
2023-05-01 12:51:09 +0100

    Loading screen added

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -2,22 +2,45 @@ import { Navigate, Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Header";
 import { userStateContext } from "../contexts/ContextProvider";
-import axiosClient from "../axios";
+import { Box, CircularProgress, Container } from "@mui/material";
+import { useEffect, useState } from "react";
 
 
 export default function DefaultLayout() {
+
+    useEffect(() => {
+        const timer = setTimeout(() => { setIsLoading(false) }, 1000);
+        return () => { clearTimeout(timer); };
+    }, []);
+
     const { currentUser, userToken } = userStateContext()
+    const [isLoading, setIsLoading] = useState(true);
 
     if (!userToken) {
         return <Navigate to='login'></Navigate>
     }
 
+    const showLoader = () => {
+        return (
+            <Box padding={'40vh 0 '} display={'flex'} alignItems={'center'} justifyContent={'center'}>
+                <CircularProgress size={150}></CircularProgress>
+            </Box>
+        );
+    };
+
+
     return (
-        <div>
-            <Header></Header>
-            <Outlet>
-            </Outlet>
-            <Footer></Footer>
-        </div>
+        <>
+            <div style={{ display: isLoading ? "block" : "none" }} >
+                {showLoader()}
+            </div >
+            <div style={{ display: isLoading ? "none" : "block" }}>
+                <Header></Header>
+                <Outlet>
+                </Outlet>
+                <Footer></Footer>
+            </div>
+        </>
+
     )
 }
